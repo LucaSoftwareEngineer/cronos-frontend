b8109ebfc47b7f1cd7a8653ff3b88937ebf5eb98
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-27 12:51:03 +0200

    feat: add button with method to allow employees to exit

--- src/app/views/presenti/presenti.ts
+++ src/app/views/presenti/presenti.ts
@@ -1,6 +1,9 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { IngressoUscita } from '../../services/ingresso-uscita';
 import { IngressoUscitaResponse } from '../../dto/response/IngressoUscitaResponse';
+import { IngressoUscitaRequest } from '../../dto/request/IngressoUscitaRequest';
+import moment from 'moment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-presenti',
@@ -11,12 +14,32 @@ import { IngressoUscitaResponse } from '../../dto/response/IngressoUscitaRespons
 export class Presenti implements OnInit {
 
   ingressoUscitaService = inject(IngressoUscita);
+  toastr = inject(ToastrService);
+
   presenze = signal<IngressoUscitaResponse[]>([]);
+  DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm';
 
   ngOnInit(): void {
+    this.getList();
+  }
+
+  getList() {
     this.ingressoUscitaService.getList().subscribe(i => {
       this.presenze.set(i);
     });
+  }
+
+  exit(id: string): void {
+
+    const request: IngressoUscitaRequest = {
+      id: id,
+      dataUscita: moment().format(this.DATE_TIME_FORMAT)
+    }
+    this.ingressoUscitaService.update(request).subscribe(res => {
+        this.toastr.success("uscita registrata correttamente...", "Successo!");
+        this.getList();
+      }
+    );
   }
 
 }
