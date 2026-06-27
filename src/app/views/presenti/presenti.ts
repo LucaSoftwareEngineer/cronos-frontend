4e8dea846f051706f27907223f4023caed691606
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-27 11:56:36 +0200

    feat: add table with list of IngressoUscita into Presenti's view

--- src/app/views/presenti/presenti.ts
+++ src/app/views/presenti/presenti.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { IngressoUscita } from '../../services/ingresso-uscita';
+import { IngressoUscitaResponse } from '../../dto/response/IngressoUscitaResponse';
 
 @Component({
   selector: 'app-presenti',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './presenti.html',
   styleUrl: './presenti.css',
 })
-export class Presenti {}
+export class Presenti implements OnInit {
+
+  ingressoUscitaService = inject(IngressoUscita);
+  presenze = signal<IngressoUscitaResponse[]>([]);
+
+  ngOnInit(): void {
+    this.ingressoUscitaService.getList().subscribe(i => {
+      this.presenze.set(i);
+    });
+  }
+
+}
