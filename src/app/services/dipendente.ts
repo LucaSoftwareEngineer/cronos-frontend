80f40e23ae66c8aac00f85dfcba64b2a2dacc66b
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-27 14:41:30 +0200

    feat: add Dipendente service with post http call to add employees

---
+++ src/app/services/dipendente.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { DipendenteRequest } from '../dto/request/DipendenteRequest';
+import { Observable } from 'rxjs';
+import { DipendenteResponse } from '../dto/response/DipendenteResponse';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Dipendente {
+
+  http = inject(HttpClient);
+
+  add(request: DipendenteRequest): Observable<DipendenteResponse> {
+    return this.http.post<DipendenteResponse>(`${environment.apiUrl}/api/dipendente/add`, request);
+  }
+
+}
